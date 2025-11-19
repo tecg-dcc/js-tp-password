7cc62801ad934d31945472ed44c284541801abb4
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-11-19 08:40:24 +0100

    :tada: Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-myApp.init();
+const btn = document.getElementById('showPass');
+const inputPassword = document.querySelector('#password');
+
+btn.addEventListener('click', ()=> {
+    if (inputPassword.type === 'text') {
+        btn.innerHTML = "Montrer"
+        inputPassword.type = 'password';
+    } else {
+        btn.textContent = "Cacher"
+        inputPassword.type = 'text';
+    }
+});
